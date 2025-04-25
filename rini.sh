7f120d7a2b05c18bb75f479ba4375d7cd8450183
Qiuyu Huang <huangq4@outlook.com>
2025-04-24 21:52:55 -0400

    add: if dirs exist

--- rini.sh
+++ rini.sh
@@ -6,15 +6,21 @@ then
     exit 1
 fi
 
+directories=("docs" "backups" "releases" "assets" "database" "src")
+
 if [[ $1 == "new" ]]
 then
-    mkdir docs
-    mkdir backups
-    mkdir releases
-    mkdir assets
-    mkdir database
-    mkdir src
-    touch readme.md
+    for i in ${directories[@]}
+    do
+        if ! [[ -d $i ]]
+        then
+            mkdir $i
+        fi
+    done
+    if ! [[ -e readme.md ]]
+    then
+        touch readme.md
+    fi
     echo 'project setup successfully'
     exit 0
 elif [[ $1 == 'm' ]]
